03eefa4e0ac2e6a9b463205c7dc4091ac261b43b
Ruzhu Chen <ruzhuchen@us.ibm.com>
2021-08-20 23:05:58 -0400

    Update 0.15.1 for WF

--- recipes/0.15.1/build.sh
+++ recipes/0.15.1/build.sh
@@ -1,2 +1,2 @@
 # Build ppc64le conda package from wheel file
-pip install /work/distributions/github/pyarrow/recipes/0.15.1/pyarrow-0.15.1-cp37-cp37m-linux_ppc64le.whl --no-deps -vvv
+pip install /work/distributions/pyarrow/0.15.1/pyarrow-0.15.1-cp37-cp37m-linux_ppc64le.whl --no-deps -vvv
